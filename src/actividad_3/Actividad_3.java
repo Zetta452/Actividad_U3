6545ebe7cacb5c83e558c947a5f734a06307127e
Zetta452 <krotdandankrot@gmail.com>
2026-04-15 11:50:10 -0500

    actulizacion final

--- src/actividad_3/Actividad_3.java
+++ src/actividad_3/Actividad_3.java
@@ -1,23 +1,30 @@
-
 package actividad_3;
 
 public class Actividad_3 {
 
     public static void main(String[] args) {
-/*  Empleado e1 = new EmpleadoAsalariado("sebastian", 6, 2000000);
+        Empleado e1 = new EmpleadoAsalariado("sebastian", 6, 2000000);
         //calulamos el salario bruto
         e1.calcularSalarioBruto();
         //calulamos el salario total
         e1.salarioNeto();
         //imprimir los datos
-        e1.imprimir();*/
-/*Empleado e2 = new EmpleadoComision(1000000, 23000000, 20, "Navarro", 2);
-e2.imprimir();*/
+        e1.imprimir();
 
-Empleado e3 = new EmpleadoTemporal(2000000, "ponton", 5);
+        System.out.println("");
+        System.out.println("*************************************");
+        Empleado e2 = new EmpleadoComision(1000000, 23000000, 20, "Navarro", 2);
+        e2.imprimir();
+
+        System.out.println("");
+        System.out.println("*************************************");
+        Empleado e3 = new EmpleadoTemporal(2000000, "ponton", 6);
         e3.imprimir();
 
-      
+        System.out.println("");
+        System.out.println("**********************************");
+        Empleado e4 = new EmpleadosHora(50, 20000, true, "Marlon", 6);
+        e4.imprimir();
+
     }
-    
 }
--- src/actividad_3/EmpleadosHora.java
+++ src/actividad_3/EmpleadosHora.java
@@ -1,11 +1,13 @@
-
 package actividad_3;
 
-public class EmpleadosHora extends Empleado{
+public class EmpleadosHora extends Empleado {
+
     //propiedades de los objeto
     private double horasTrabajadas;
     private double tarifaHora;
     private boolean aceptarFondo;
+    private double extras;
+    private double valorExtra;
 
     //constructor del objeto
     public EmpleadosHora(double horasTrabajadas, double tarifaHora, boolean aceptarFondo, String nombre, int tiempoEmpresa) {
@@ -13,52 +15,68 @@ public class EmpleadosHora extends Empleado{
         this.horasTrabajadas = horasTrabajadas;
         this.tarifaHora = tarifaHora;
         this.aceptarFondo = aceptarFondo;
-        
+        this.calcularSalarioBruto();
+
         //si las horas ingresadas son negativas se estabecen como 0
-        if (horasTrabajadas <0){
-        horasTrabajadas = 0;
+        if (horasTrabajadas < 0) {
+            horasTrabajadas = 0;
         }
-        
-       }
-    
-            @Override
+
+    }
+
+    @Override
     public double calcularSalarioBruto() {
-        
+
         //variable para almacenar el salario
         double salario;
-        
+        this.valorExtra = 0;
+
         //Verificar si el trabajador cumple con mas de las 40 horas trabajadas
-        if(horasTrabajadas > 40){
-            
+        if (horasTrabajadas > 40) {
+
             //conocer las horas extras excactas que se hizo el trabajo
-            double extras = horasTrabajadas - 40;
-            
+            this.extras = horasTrabajadas - 40;
+
             //conocer el salario que se le debe pagar junto a las horas extras
-            salario = (40 * tarifaHora) + (extras * tarifaHora * 1.5);
-            
+            valorExtra = extras * tarifaHora * 1.5;
+
+            salario = (40 * tarifaHora) + valorExtra;
+
             //si no cuemple con el veneficio de las horas estras se facturan las 40 horas
-        }else {
-        
+        } else {
+
             //multiplicamos las horas trabajadas por la tarifa por hora
             salario = horasTrabajadas * tarifaHora;
-          
+
         }
-        
+
         // se verifiac si tiene mas de un año de permanecia y si acepta el acceso al fondo de pension
-        if(tiempoEmpresa > 1 && aceptarFondo){
-        
+        if (tiempoEmpresa > 1 && aceptarFondo) {
+
             //operacion para almacenar el 2 % del salario depositado
             salario += salario * 0.02;
         }
         return salario;
-        }
     }
-        
-    
-    
-    
 
-    
-            
-    
+    @Override
+    public void imprimir() {
+        System.out.println("Empleado por horas");
+        System.out.println("Nombre = " + this.nombre);
+        System.out.println("Horas trabajadas = " + this.horasTrabajadas);
+        System.out.println("Valor por hora = " + this.tarifaHora);
+        if (horasTrabajadas > 40) {
+            System.out.println("horas extas son = " + this.extras);
+        }
+        System.out.println("___________________________________________");
+        System.out.println("ARL = -$" + retencionarl(calcularSalarioBruto()));
+        System.out.println("SALUD = -$" + retencionSalud(calcularSalarioBruto()));
+        System.out.println("Salario bruto = $" + calcularSalarioBruto());
+        if (horasTrabajadas > 40) {
+            System.out.println("Valor por horas extras = " + this.valorExtra);
+        }
 
+        System.out.println("_____________________________________________");
+        System.out.println("Salario neto a pagar = $" + salarioNeto());
+    }
+}
